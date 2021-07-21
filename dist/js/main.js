15ab2160ef74e5242726ed8f0d482e51d279f73d
radojicicBojan <bojanradojicic00@gmail.com>
2021-07-21 20:53:14 +0200

    - Footer

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,15 @@
-//alert("NESTO");
+console.log("Right  clicked");
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vYWxlcnQoXCJORVNUT1wiKTtcclxuIl0sImZpbGUiOiJtYWluLmpzIn0=
+let RightButton = document.getElementById("RightControl");
+let LeftButton = document.getElementById("LeftControl");
+
+RightButton.addEventListener("click", function(){
+    console.log("Right button clicked");
+})
+
+LeftButton.addEventListener("click", function(){
+    console.log("Left button clicked");
+})
+
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiUmlnaHQgIGNsaWNrZWRcIik7XHJcblxyXG5sZXQgUmlnaHRCdXR0b24gPSBkb2N1bWVudC5nZXRFbGVtZW50QnlJZChcIlJpZ2h0Q29udHJvbFwiKTtcclxubGV0IExlZnRCdXR0b24gPSBkb2N1bWVudC5nZXRFbGVtZW50QnlJZChcIkxlZnRDb250cm9sXCIpO1xyXG5cclxuUmlnaHRCdXR0b24uYWRkRXZlbnRMaXN0ZW5lcihcImNsaWNrXCIsIGZ1bmN0aW9uKCl7XHJcbiAgICBjb25zb2xlLmxvZyhcIlJpZ2h0IGJ1dHRvbiBjbGlja2VkXCIpO1xyXG59KVxyXG5cclxuTGVmdEJ1dHRvbi5hZGRFdmVudExpc3RlbmVyKFwiY2xpY2tcIiwgZnVuY3Rpb24oKXtcclxuICAgIGNvbnNvbGUubG9nKFwiTGVmdCBidXR0b24gY2xpY2tlZFwiKTtcclxufSlcclxuXHJcbiJdLCJmaWxlIjoibWFpbi5qcyJ9
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-//alert("NESTO");
+console.log("Right  clicked");
+
+let RightButton = document.getElementById("RightControl");
+let LeftButton = document.getElementById("LeftControl");
+
+RightButton.addEventListener("click", function(){
+    console.log("Right button clicked");
+})
+
+LeftButton.addEventListener("click", function(){
+    console.log("Left button clicked");
+})
+
